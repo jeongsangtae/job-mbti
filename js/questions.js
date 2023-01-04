41dd590e9f0e68ed8a52a639350be1dff8fb4cb8
jeongsangtae <wjdtkdxo55@naver.com>
2023-01-04 23:50:08 +0900

    questions js part2

--- js/questions.js
+++ js/questions.js
@@ -18,4 +18,31 @@ function renderQuestion() {
   progressValueEl.style.width = (currentNumber + 1) * 10 + "%";
 }
 
+function nextQuestion(choiceNumber) {
+  if (currentNumber === questions.length - 1) {
+    showResultPage();
+    return;
+  }
+  const question = questions[currentNumber];
+  mbti = mbti + question.choices[choiceNumber].value;
+  // mbti = '' + 'e' or "i"
+  // mbti = 'i' + 's' or 'n'
+  // mbti = 'is' + 't' or 'f'
+  // mbti = 'ist' + 'j' or 'p'
+  // mbti = 'istj'
+  currentNumber = currentNumber + 1;
+  renderQuestion();
+}
+
+function showResultPage() {
+  location.href = "/results.html?mbti=" + mbti; // 쿼리스트링
+}
+
+choice1El.addEventListener("click", function () {
+  nextQuestion(0);
+});
+choice2El.addEventListener("click", function () {
+  nextQuestion(1);
+});
+
 renderQuestion();
