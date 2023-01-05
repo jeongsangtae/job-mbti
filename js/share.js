938a40567be603308d3ecdc3810dc8b2e07ed736
jeongsangtae <wjdtkdxo55@naver.com>
2023-01-05 23:38:32 +0900

    Open Graph, Twitter Card meta hmtl

--- js/share.js
+++ js/share.js
@@ -14,8 +14,8 @@ document.body.append(healthEl);
 // 모바일 브라우저 내장 공유 기능!
 async function startNativeShare() {
   await navigator.share({
-    title: "내 안에 숨어있는 직업캐 찾기!",
-    text: "누구나 찰떡인 직업이 있어요! 내 안에 숨어있는 직업캐를 찾아보세요!",
+    title: "내 안에 숨어있는 직업 찾기!",
+    text: "누구나 찰떡인 직업이 있어요! 내 안에 숨어있는 직업을 찾아보세요!",
     url: location.href, // 현재 페이지 주소!
   });
 }
